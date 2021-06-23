f179fa401d5f2fc995d7ec7f8d3eadd257535308
kmiodragovic <302687697@student.rocmondriaan>
2021-06-23 22:19:37 +0200

    theme af

--- assets/js/main.js
+++ assets/js/main.js
@@ -57,48 +57,39 @@ sr.reveal('.work__img',{interval: 200});
 sr.reveal('.contact__input',{interval: 200}); 
 
 //  function to set a given theme/color-scheme
- function setTheme(themeName) {
-    localStorage.setItem('theme', '.theme-light');
-   document.documentElement.className = '.theme-light';
+
+// Get the theme toggle input
+const themeToggle = document.querySelector(
+'.theme-switch input[type="checkbox"]'
+);
+
+// Get the current theme from local storage
+const currentTheme = localStorage.getItem("theme");
+
+// If the current local storage item can be found
+if (currentTheme) {
+// Set the body data-theme attribute to match the local storage item
+document.documentElement.setAttribute("data-theme", currentTheme);
+
+// If the current theme is dark, check the theme toggle
+if (currentTheme === "light") {
+    themeToggle.checked = true;
+}
 }
 
-// themeSwitch.addEventListener('change', () => {
-//     document.body.classList.toggle('theme-dark');
-//   });
-
-// function to toggle between light and dark theme
-function toggleTheme() {
-    if (localStorage.getItem('theme') === '.theme-dark') {
-        setTheme('.theme-light');
-    } else {
-        setTheme('.theme-dark');
-    }
+// Function that will switch the theme based on the if the theme toggle is checked or not
+function switchTheme(e) {
+if (e.target.checked) {
+    document.documentElement.setAttribute("data-theme", "light");
+    localStorage.setItem("theme", "dark");
+} else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "light");
+}
 }
 
-// Immediately invoked function to set the theme on initial load
-(function toggleTheme() {
-    if (localStorage.getItem('theme') === '.theme-dark') {
-        setTheme('.theme-dark');
-        document.getElementById('slider').checked = false;
-    } else (localStorage.getItem('theme') === '.theme-light')
-        setTheme('.theme-light');
-      document.getElementById('slider').checked = true;
-    }
-)();
+// Add an event listener to the theme toggle, which will switch the theme
+themeToggle.addEventListener("change", switchTheme, false);
 
-// Themeswitcher with timer
-setInterval(function(){
-  
-    const currentDate = new Date();
 
-    if('.theme-light' === false){
-        if(currentDate.getHours() >= 7 && currentDate.getHours() <= 20 ){
-                changeHandler();
-                console.log(currentDate.getHours());
-        }
-    }
-    else if('.theme-light' === true){
-        console.log(currentDate.getHours());
-    }
 
-}, 1000);
