720afd58a1fff21d0e00861c2508fc014eb25045
kmiodragovic <302687697@student.rocmondriaan>
2021-06-23 15:13:35 +0200

    metTheme

--- assets/js/main.js
+++ assets/js/main.js
@@ -56,30 +56,49 @@ sr.reveal('.work__img',{interval: 200});
 /*SCROLL CONTACT*/
 sr.reveal('.contact__input',{interval: 200}); 
 
-/*===== Theme Switcher =====*/
-
- // function to set a given theme/color-scheme
+//  function to set a given theme/color-scheme
  function setTheme(themeName) {
-    localStorage.setItem('theme', themeName);
-    document.documentElement.className = themeName;
+    localStorage.setItem('theme', '.theme-light');
+   document.documentElement.className = '.theme-light';
 }
+
+// themeSwitch.addEventListener('change', () => {
+//     document.body.classList.toggle('theme-dark');
+//   });
 
 // function to toggle between light and dark theme
 function toggleTheme() {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-light');
+    if (localStorage.getItem('theme') === '.theme-dark') {
+        setTheme('.theme-light');
     } else {
-        setTheme('theme-dark');
+        setTheme('.theme-dark');
     }
 }
 
 // Immediately invoked function to set the theme on initial load
-(function () {
-    if (localStorage.getItem('theme') === 'theme-dark') {
-        setTheme('theme-dark');
+(function toggleTheme() {
+    if (localStorage.getItem('theme') === '.theme-dark') {
+        setTheme('.theme-dark');
         document.getElementById('slider').checked = false;
-    } else {
-        setTheme('theme-light');
+    } else (localStorage.getItem('theme') === '.theme-light')
+        setTheme('.theme-light');
       document.getElementById('slider').checked = true;
     }
-})();
+)();
+
+// Themeswitcher with timer
+setInterval(function(){
+  
+    const currentDate = new Date();
+
+    if('.theme-light' === false){
+        if(currentDate.getHours() >= 7 && currentDate.getHours() <= 20 ){
+                changeHandler();
+                console.log(currentDate.getHours());
+        }
+    }
+    else if('.theme-light' === true){
+        console.log(currentDate.getHours());
+    }
+
+}, 1000);
