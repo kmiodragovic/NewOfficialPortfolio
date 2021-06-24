c695084c54eab6b459d6bb4b23ae1bc7ae99374c
kmiodragovic <302687697@student.rocmondriaan>
2021-06-25 01:30:06 +0200

    little changes and fixes

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,66 +1,66 @@
-/*===== MENU =====*/ 
-const showMenu = (toggleId, navId) =>{
-    const toggle = document.getElementById(toggleId),
-    nav = document.getElementById(navId)
+/*===== MENU =====*/
+const showMenu = (toggleId, navId) => {
+  const toggle = document.getElementById(toggleId),
+    nav = document.getElementById(navId);
 
-    if(toggle && nav){
-        toggle.addEventListener('click', ()=>{
-            nav.classList.toggle('show')
-        })
-    }
-}
-showMenu('nav-toggle','nav-menu')
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+};
+showMenu("nav-toggle", "nav-menu");
 
 /*===== ACTIVE AND REMOVE MENU =====*/
-const navLink = document.querySelectorAll('.nav__link');   
+const navLink = document.querySelectorAll(".nav__link");
 
-function linkAction(){
+function linkAction() {
   /*Active link*/
-  navLink.forEach(n => n.classList.remove('active'));
-  this.classList.add('active');
-  
+  navLink.forEach((n) => n.classList.remove("active"));
+  this.classList.add("active");
+
   /*Remove menu mobile*/
-  const navMenu = document.getElementById('nav-menu')
-  navMenu.classList.remove('show')
+  const navMenu = document.getElementById("nav-menu");
+  navMenu.classList.remove("show");
 }
-navLink.forEach(n => n.addEventListener('click', linkAction));
+navLink.forEach((n) => n.addEventListener("click", linkAction));
 
 /*===== SCROLL ANIMATION =====*/
 const sr = ScrollReveal({
-    origin: 'top',
-    distance: '80px',
-    duration: 2000,
-    reset: true
+  origin: "top",
+  distance: "80px",
+  duration: 2000,
+  reset: true,
 });
 
 /*SCROLL HOME*/
-sr.reveal('.home__title',{}); 
-sr.reveal('.button',{delay: 200}); 
-sr.reveal('.home__img',{delay: 400}); 
-sr.reveal('.home__social-icon',{ interval: 200}); 
+sr.reveal(".home__title", {});
+sr.reveal(".button", { delay: 200 });
+sr.reveal(".home__img", { delay: 400 });
+sr.reveal(".home__social-icon", { interval: 200 });
 
 /*SCROLL ABOUT*/
-sr.reveal('.about__img',{}); 
-sr.reveal('.about__subtitle',{delay: 400}); 
-sr.reveal('.about__text',{delay: 400}); 
+sr.reveal(".about__img", {});
+sr.reveal(".about__subtitle", { delay: 400 });
+sr.reveal(".about__text", { delay: 400 });
 
 /*SCROLL SKILLS*/
-sr.reveal('.skills__subtitle',{}); 
-sr.reveal('.skills__text',{}); 
-sr.reveal('.skills__data',{interval: 200}); 
-sr.reveal('.skills__img',{delay: 600});
+sr.reveal(".skills__subtitle", {});
+sr.reveal(".skills__text", {});
+sr.reveal(".skills__data", { interval: 200 });
+sr.reveal(".skills__img", { delay: 600 });
 
 /*SCROLL WORK*/
-sr.reveal('.work__img',{interval: 200}); 
+sr.reveal(".work__img", { interval: 200 });
 
 /*SCROLL CONTACT*/
-sr.reveal('.contact__input',{interval: 200}); 
+sr.reveal(".contact__input", { interval: 200 });
 
-sr.reveal('.checkbox'),{interval: 200};
+sr.reveal(".checkbox"), { interval: 200 };
 
 // Get the theme toggle input
 const themeToggle = document.querySelector(
-'.theme-switch input[type="checkbox"]'
+  '.theme-switch input[type="checkbox"]'
 );
 
 // Get the current theme from local storage
@@ -68,29 +68,25 @@ const currentTheme = localStorage.getItem("theme");
 
 // If the current local storage item can be found
 if (currentTheme) {
-// Set the body data-theme attribute to match the local storage item
-document.documentElement.setAttribute("data-theme", currentTheme);
+  // Set the body data-theme attribute to match the local storage item
+  document.documentElement.setAttribute("data-theme", currentTheme);
 
-// If the current theme is dark, check the theme toggle
-if (currentTheme === "light") {
+  // If the current theme is dark, check the theme toggle
+  if (currentTheme === "light") {
     themeToggle.checked = true;
-}
+  }
 }
 
 // Function that will switch the theme based on the if the theme toggle is checked or not
 function switchTheme(e) {
-if (e.target.checked) {
+  if (e.target.checked) {
     document.documentElement.setAttribute("data-theme", "light");
-    localStorage.setItem("theme", "dark");
-} else {
-    document.documentElement.setAttribute("data-theme", "dark");
     localStorage.setItem("theme", "light");
-}
+  } else {
+    document.documentElement.setAttribute("data-theme", "dark");
+    localStorage.setItem("theme", "dark");
+  }
 }
 
 // Add an event listener to the theme toggle, which will switch the theme
 themeToggle.addEventListener("change", switchTheme, true);
-
-
-
-
